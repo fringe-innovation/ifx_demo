e063c8461371d669e86aa0d6cdb413f080ffd015
YWJCJ <ywjcj@foxmail.com>
2023-04-10 20:09:15 +0800

    add chirpfft function

--- chirp_fft.py
+++ chirp_fft.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+def chirp1dfft(numsample, numchirp, numframe, data):
+    win = np.hamming(numsample)
+    range = np.zeros((numsample, numchirp * numframe))
+    for i in range(numchirp * numframe):
+        temp = data[:, i] * win  # 将data矩阵第i列与汉明窗win逐元素相乘
+        temp = np.fft.fft(temp, numsample)  # 对temp向量做FFT变换
+        range[:, i] = temp  # 存储FFT变换结果到range矩阵的第i列
