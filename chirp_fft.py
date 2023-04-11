9192c12f3c41c9e8ef113c34872e8306c45ac63f
YWJCJ <ywjcj@foxmail.com>
2023-04-11 20:55:41 +0800

    add in chirpfft

--- chirp_fft.py
+++ chirp_fft.py
@@ -2,8 +2,16 @@ import numpy as np
 
 def chirp1dfft(numsample, numchirp, numframe, data):
     win = np.hamming(numsample)
-    range = np.zeros((numsample, numchirp * numframe))
+    r = np.zeros((numsample, numchirp * numframe))
     for i in range(numchirp * numframe):
         temp = data[:, i] * win  # 将data矩阵第i列与汉明窗win逐元素相乘
         temp = np.fft.fft(temp, numsample)  # 对temp向量做FFT变换
-        range[:, i] = temp  # 存储FFT变换结果到range矩阵的第i列
+        r[:, i] = temp  # 存储FFT变换结果到range矩阵的第i列
+
+        a = 0.625  # 加权因子
+        b = np.zeros(numsample)
+        temp = np.zeros((numsample, numchirp * numframe))
+        for i in range(numframe):
+            h = range[:, i]  # 取出range矩阵的第i列
+            b = a * b + (1 - a) * h  # 更新b向量
+            temp[:, i] = h - b  # 计算并存储当前帧与平均值之间的差值
